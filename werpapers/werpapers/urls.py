2dd08f553d16273388b8283b9059d50daa739d3d
Gustavo Fonseca Olenka <gustavo_olenka@hotmail.com>
2018-07-04 17:23:44 -0300

    testando pra ver se vai dar merda

--- werpapers/werpapers/urls.py
+++ werpapers/werpapers/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# from django.conf.urls import url
 
 urlpatterns = [
     path('werpapers/', include('werpapers_manager.urls')),
     path('admin/', admin.site.urls),
+    # url(r'^', include('website.urls', namespace='website'))
 ]
--- werpapers/werpapers_manager/scrapeBS4.py
+++ werpapers/werpapers_manager/scrapeBS4.py
@@ -3,29 +3,23 @@
 from bs4 import BeautifulSoup as bs
 import requests
 
-def pegarNumeroDownloads():
+# link para o artigo 2 do wer98
+scholar_url1 = 'https://scholar.google.com.br/scholar?as_q=%22derivaci'
+scholar_url2 = '%C3%B3n%20de%20objetos%20utilizando%20lel%20y%20escenarios%20en%20un%20caso%20'
+scholar_url3 = 'real%22&as_sauthors=%22fresno%22&num=50&hl=pt-BR&lr=&btnG=Pesquisar&lr='
+scholar_url: str = scholar_url1 + scholar_url2 + scholar_url3
 
-    # link para o artigo 2 do wer98
-    scholar_url1 = 'https://scholar.google.com.br/scholar?as_q=%22derivaci'
-    scholar_url2 = '%C3%B3n%20de%20objetos%20utilizando%20lel%20y%20escenarios%20en%20un%20caso%20'
-    scholar_url3 = 'real%22&as_sauthors=%22fresno%22&num=50&hl=pt-BR&lr=&btnG=Pesquisar&lr='
-    scholar_url: str = scholar_url1 + scholar_url2 + scholar_url3
+# transform
+page = requests.get(scholar_url)
+html_contents = page.text
 
-    # transform
-    page = requests.get(scholar_url)
-    html_contents = page.text
+page_soup_htmlparser = bs(html_contents, 'html.parser')
 
-    page_soup_htmlparser = bs(html_contents, 'html.parser')
+containers = page_soup_htmlparser.findAll(name='div', attrs={'class': 'gs_fl'})
 
-    containers = page_soup_htmlparser.findAll(name='div', attrs={'class': 'gs_fl'})
+# seleção do texto desejado no container
+amount_articles = containers[2].text
+amount = [s for s in amount_articles.split() if s.isdigit()]
 
-    # seleção do texto desejado no container
-    amount_articles = containers[2].text
-    amount = [s for s in amount_articles.split() if s.isdigit()]
-
-    return amount[0]
-
-# print(amount[0])
-
-# o numero desejado se encontra em amount[0], a primeira posicao da lista
+#o numero desejado se encontra em amount[0], a primeira posicao da lista
 
